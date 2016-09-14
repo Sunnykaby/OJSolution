a6a4f1d14241e017585df54d6a8dd46e52b6b958
kami <sunnykaby@163.com>
2016-09-14 14:01:40 +0800

    leetcode 116

---
+++ src/com/kami/leetcode/Leetcode_116.java
@@ -0,0 +1,14 @@
+package com.kami.leetcode;
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Leetcode_116 {
+	public void connect(TreeLinkNode root) {
+		
+	}
+}
