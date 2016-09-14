edf469ae56a0b5afa0e2318eb6c63fff312ed134
kami <sunnykaby@163.com>
2016-09-14 14:03:53 +0800

    fix 116 bug

--- src/com/kami/leetcode/Leetcode_116.java
+++ src/com/kami/leetcode/Leetcode_116.java
@@ -1,4 +1,10 @@
 package com.kami.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
 /**
  * Definition for binary tree with next pointer.
  * public class TreeLinkNode {
@@ -9,6 +15,33 @@ package com.kami.leetcode;
  */
 public class Leetcode_116 {
 	public void connect(TreeLinkNode root) {
-		
+		//here we use the queue and BFS to do this job
+		int curLine = 0, nextLine = 0;
+		Queue<TreeLinkNode> nodeQueue = new LinkedList<>();
+		if (root == null) {
+			return;
+		}
+		nodeQueue.add(root);
+		curLine = 1;
+		while(!nodeQueue.isEmpty()){
+			while(curLine > 0){
+				TreeLinkNode cur = nodeQueue.poll();
+				if (cur == null) {
+					return;
+				}
+				if (cur.left != null)  {nodeQueue.add(cur.left);nextLine++;}
+				if (cur.right != null) {nodeQueue.add(cur.right);nextLine++;}
+				if (curLine == 1) {
+					cur.next = null;
+				}
+				else cur.next = nodeQueue.peek();
+				curLine--;
+			}
+			if (nextLine != 0) {
+				curLine = nextLine;
+				nextLine = 0;
+			}
+		}
 	}
 }
+ 
